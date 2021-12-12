f3bb2b0c4ecea38f42ec54b8eabc07b11f60f8b1
aminpt <94472531+aminpt@users.noreply.github.com>
2021-12-12 22:21:53 +0330

    Create contact.js

---
+++ contact.js
@@ -0,0 +1,8 @@
+function send()
+{ var c = confirm(" از ارسال پیام خود مطمئنید؟");
+        if(c == true)
+                alert("پیام ارسال شد:)") ;
+        else
+                alert("پیام ارسال نشد:/");
+
+}
